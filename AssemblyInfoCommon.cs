d91df9bde44150ba1dbaf690a5156aa0632d3191
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-05-13 14:55:27 +0300

    Minipie: version upgrade, installer now correctly uninstalls previous versions

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -4,5 +4,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyVersion("0.0.1.1")]
-[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyVersion("0.0.1.2")]
+[assembly: AssemblyFileVersion("0.0.1.2")]
