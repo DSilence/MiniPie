4988386cd834bcef17bfd54aaf1768a77cf603ff
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-07-08 15:59:33 +0300

    Fix: installation dialog.

--- MiniPie.Core/ClickOnceHelpers/ClickOnceHelper.cs
+++ MiniPie.Core/ClickOnceHelpers/ClickOnceHelper.cs
@@ -199,8 +199,10 @@ namespace MiniPie.ClickOnceHelpers
             PostMessage(myWindowHandle, WM_KEYDOWN, (IntPtr)Keys.Tab, (IntPtr)wparam);
             //PostMessage(myWindowHandle, WM_KEYUP, (IntPtr)Keys.Tab, (IntPtr)wparam);*/
 
-            PostMessage(myWindowHandle, WM_KEYDOWN, (IntPtr)Keys.Enter, (IntPtr)wparam);
-            //PostMessage(myWindowHandle, WM_KEYUP, (IntPtr)Keys.Enter, (IntPtr)wparam);
+            //PostMessage(myWindowHandle, WM_KEYDOWN, (IntPtr)Keys.Enter, (IntPtr)wparam);
+            PostMessage(myWindowHandle, WM_KEYUP, (IntPtr)Keys.Tab, (IntPtr)wparam);
+            PostMessage(myWindowHandle, WM_KEYUP, (IntPtr)Keys.Tab, (IntPtr)wparam);
+            PostMessage(myWindowHandle, WM_KEYUP, (IntPtr)Keys.Enter, (IntPtr)wparam);
         }
         #endregion
     }
