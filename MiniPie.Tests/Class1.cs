df6e3f9773a5181ba567cd34ac930c5af04da380
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-08-19 00:33:42 +0300

    Added: unit tests.

---
+++ MiniPie.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiniPie.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 2+2);
+        }
+    }
+}
