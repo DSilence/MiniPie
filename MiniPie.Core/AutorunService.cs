753e0d8872f71d66b9560683744c5f192b1a5173
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-08-25 23:36:54 +0300

    Fixed issue [MP-2]: Start with windows doesn't work.

--- MiniPie.Core/AutorunService.cs
+++ MiniPie.Core/AutorunService.cs
@@ -1,44 +1,65 @@
 ﻿using System;
-using System.Windows;
-using Microsoft.Win32;
+using System.Deployment.Application;
+using System.IO;
 
-namespace MiniPie.Core {
-    public sealed class AutorunService {
+namespace MiniPie.Core
+{
+    public sealed class AutorunService
+    {
         private readonly ILog _Logger;
         private readonly AppSettings _Settings;
         private readonly AppContracts _Contracts;
-        private readonly RegistryKey _AutorunHive;
 
         private const string AutorunSettingsName = "StartWithWindows";
-        
-        public AutorunService(ILog logger, AppSettings settings, AppContracts contracts) {
+        private const string Extention = ".appref-ms";
+
+        public AutorunService(ILog logger, AppSettings settings, AppContracts contracts)
+        {
             _Logger = logger;
             _Settings = settings;
             _Contracts = contracts;
-            _AutorunHive = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
 
             ValidateAutorun();
 
-            _Settings.PropertyChanged += (o, e) => {
-                                             if (e.PropertyName == AutorunSettingsName)
-                                                 ValidateAutorun();
-                                         };
+            _Settings.PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == AutorunSettingsName)
+                    ValidateAutorun();
+            };
         }
 
-        private void ValidateAutorun() {
-            try {
-                if (_Settings.StartWithWindows) { // Add/Update autorun
-                    var currentValue = (string) _AutorunHive.GetValue(_Contracts.ApplicationName, string.Empty);
-                    var currentLocation = Application.ResourceAssembly.Location;
-                    if(currentValue != currentLocation)
-                        _AutorunHive.SetValue(_Contracts.ApplicationName, currentLocation);
+        private void ValidateAutorun()
+        {
+            try
+            {
+                string startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+                startupPath = Path.Combine(startupPath, _Contracts.ProductName) + Extention;
+                if (_Settings.StartWithWindows)
+                {
+                    // Add/Update autorun
+                    if (ApplicationDeployment.IsNetworkDeployed)
+                    {
+                        if (!File.Exists(startupPath))
+                        {
+                            string allProgramsPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
+                            string shortcutPath = Path.Combine(allProgramsPath, _Contracts.PublisherName);
+                            shortcutPath = Path.Combine(shortcutPath, _Contracts.ProductName) + Extention;
+
+                            File.Copy(shortcutPath, startupPath);
+                        }
+                    }
                 }
-                else { //Remove autorun
-                    if (!string.IsNullOrEmpty((string) _AutorunHive.GetValue(_Contracts.ApplicationName, string.Empty)))
-                        _AutorunHive.DeleteValue(_Contracts.ApplicationName, false);
+                else
+                {
+                    //Remove autorun
+                    if (File.Exists(startupPath))
+                    {
+                        File.Delete(startupPath);
+                    }
                 }
             }
-            catch (Exception exc) {
+            catch (Exception exc)
+            {
                 _Logger.FatalException("Failed to update or remove autorun", exc);
             }
         }
