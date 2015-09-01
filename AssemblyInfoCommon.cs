8c3ba7c0decbb3e24f269591eefcc2514ff7d725
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-09-01 12:48:58 +0300

    Updated: version

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -4,5 +4,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyVersion("0.0.2.3")]
-[assembly: AssemblyFileVersion("0.0.2.3")]
+[assembly: AssemblyVersion("0.0.2.4")]
+[assembly: AssemblyFileVersion("0.0.2.4")]
