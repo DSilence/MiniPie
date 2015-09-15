5c52abc6927414723ac1e2ab7910c581265b214a
Dzmitry Safarau <Dzmitry_Safarau@outlook.com>
2015-09-15 10:42:09 +0300

    Fix: fix for playlist items.

--- MiniPie/Converter/PlaylistMenuItemsConverter.cs
+++ MiniPie/Converter/PlaylistMenuItemsConverter.cs
@@ -8,27 +8,37 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Data;
 using MiniPie.Core.SpotifyWeb.Models;
+using MiniPie.ViewModels;
 
 namespace MiniPie.Converter
 {
-    public class PlaylistMenuItemsConverter: IValueConverter
+    public class PlaylistMenuItemsConverter: IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var playlists = value as ObservableCollection<Playlist>;
-            if (playlists == null)
+            var playlists = values[0] as ObservableCollection<Playlist>;
+            var viewModel = values[1] as ShellViewModel;
+            if (playlists == null || viewModel == null)
             {
                 return null;
             }
             var result = new List<MenuItem>(playlists.Count);
             foreach (var playlist in playlists)
             {
-                
+                var menuItem = new MenuItem
+                {
+                    Header = playlist.Name,
+                };
+                menuItem.Click += (sender, args) =>
+                {
+                    viewModel.AddToPlaylist(playlist.Id);
+                };
+                result.Add(menuItem);
             }
             return result;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
