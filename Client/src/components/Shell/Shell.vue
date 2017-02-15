0ec38aa026ba10b8ffcd3f5ea7c6c07ae09242fe
Dzmitry Safarau <DSilencea@gmail.com>
2017-02-15 22:54:12 +0300

    Added: initial vue stuff

---
+++ Client/src/components/Shell/Shell.vue
@@ -0,0 +1,8 @@
+<div>
+    <img :src="albumArtUrl"/>
+    <div id="buttonContainer">
+        <md-button></md-button>
+        <md-button></md-button>
+        <md-button></md-button>
+    </div>
+</div>
---
+++ Client/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import App from './App'
+import router from './router'
+
+Vue.use(VueMaterial)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
